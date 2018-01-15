d6e28c798a4299ee80a7ad527aaa17618d2b44d4
Feihong Hsu <feihong.hsu@gmail.com>
2018-01-14 23:18:46 -0600

    Add move-showtimes function

--- env/dev/clj/user.clj
+++ env/dev/clj/user.clj
@@ -3,7 +3,9 @@
             [movies.config :refer [env]]
             [mount.core :as mount]
             [movies.core :refer [start-app]]
-            [org.httpkit.client :as http]))
+            [org.httpkit.client :as http]
+            [clj-time.core]
+            [clj-time.format :as f]))
 
 
 (defn start []
@@ -24,3 +26,14 @@
 
 (defn create-migration [name]
   (migrations/create name (select-keys env [:database-url])))
+
+(defn movie-showtimes []
+  (let [url "http://data.tmsapi.com/v1.1/movies/showings"
+        date-str (-> (f/formatters :date)
+                     (f/unparse (clj-time.core/now)))
+        params {:startDate date-str
+                :api_key (-> env :gracenote :api-key)
+                :lat 41.891377
+                :lng -87.618997}
+        res @(http/get url {:query-params params})]
+    (:body res)))
